31431ac54bc03794eb555eff4abf9e773dbbae50
DAMIAN283 <dilandamian283@gmail.com>
2026-04-23 01:05:04 -0500

    feat: Terminamos clase categoriaDAO

--- src/datos/CategoriaDAO.java
+++ src/datos/CategoriaDAO.java
@@ -41,36 +41,121 @@ public class CategoriaDAO implements CrudSimpleInterface<Categorias>{
               while(rs.next()){
                   registros.add(new Categorias(rs.getInt(1),rs.getString(2), rs.getString(3), rs.getBoolean(4)));
               }
+              ps.close();
+              rs.close();
          } catch (SQLException e) {
              JOptionPane.showMessageDialog(null, e.getMessage());
          }
+         finally{
+        ps=null;
+        rs=null;
+        CON.desconectar();
+    }
          return registros;
     }
 
     @Override
     public boolean insertar(Categorias obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        resp=false;
+        try {
+            ps=CON.conectar().prepareStatement("ISERT INTO categorias (nombre,descripcion, activo) VALUES (?,?,1");
+            ps.setString(1, obj.getNombre());
+            ps.setString(2, obj.getDescripcion());
+            if(ps.executeUpdate()> 0){
+                resp= true;
+            }
+            ps.close();;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        finally{
+            ps=null;
+            CON.desconectar();
+        }
+        return resp;
+     }
 
     @Override
     public boolean actualizar(Categorias obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        resp=false;
+        try {
+            ps=CON.conectar().prepareStatement("UPDATE categoria SET activo=0, WHERE id=?");
+            ps.setString(1, obj.getNombre());
+            ps.setString(2, obj.getDescripcion());
+            ps.setInt(3, obj.getId());
+            if(ps.executeUpdate()> 0){
+                resp= true;
+            }
+            ps.close();;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        finally{
+            ps=null;
+            CON.desconectar();
+        }
+        return resp;
+     }
 
     @Override
     public boolean desactivar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        resp=false;
+        try {
+            ps=CON.conectar().prepareStatement("UPDATE categoria SET activo=0, WHERE id=?");
+            ps.setInt(1, id);
+            if(ps.executeUpdate()> 0){
+                resp= true;
+            }
+            ps.close();;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        finally{
+            ps=null;
+            CON.desconectar();
+        }
+        return resp;
+        }
 
     @Override
-    public boolean activar(int obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public boolean activar(int id) {
+        resp=false;
+        try {
+            ps=CON.conectar().prepareStatement("UPDATE categoria SET nombre=?, descripcion=? WHERE id=?");
+            ps.setInt(1,id);
+            if(ps.executeUpdate()> 0){
+                resp= true;
+            }
+            ps.close();;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        finally{
+            ps=null;
+            CON.desconectar();
+        }
+        return resp;
+        }
 
     @Override
     public int total() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        int totalRegistros=0;
+        try {
+            ps=CON.conectar().prepareStatement("SELECT count(id) categorias");
+            rs=ps.executeQuery();
+            while(rs.next()){
+                totalRegistros=rs.getInt("COUNT(id)");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        finally{
+            ps=null;
+            CON.desconectar();
+        }
+        return totalRegistros;
+          }
 
     @Override
     public boolean exite(String texto) {
